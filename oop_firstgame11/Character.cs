e6a736ce34e1d170afa37f4927e7587158039c27
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2022-03-29 11:34:38 +0200

    Add project files.

---
+++ oop_firstgame11/Character.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_firstgame11
+{
+    class Character
+    {
+        private int shield;
+        private int health;
+        private int damage;
+        public string Name { get; }
+        public int Health { get => health; }
+        public int Shield { get => shield;}
+        public int Damage { get => damage;}
+
+        public Character(string name)
+        {
+            Name = name;
+            health = 100;
+            shield = 50;
+            damage = 20;
+
+        }
+
+        public void GetHit(int damage)
+        {
+            shield -= damage;
+            if(shield < 0)
+            {
+                health += shield;
+                shield = 0;
+
+
+            }
+
+        }
+        public void Heal()
+        {
+            health += 15;
+        }
+        public override string ToString()
+        {
+            string text = "Name" + Name + "\n";
+            text += "Shield" + shield + "\n";
+            text += "Health" + health + "\n";
+            text += "Damage" + damage + "\n";
+
+            return text;
+        }
+    }
+}
---
+++ oop_firstgame11/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oop_firstgame11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Character character1;
+        Character character2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            character1 = new Character("Petr");
+            character2 = new Character("Pavel");
+            txt1.Text = character1.ToString();
+            txt2.Text = character2.ToString();
+        }
+
+        private void attack_Click(object sender, RoutedEventArgs e)
+        {
+            character2.GetHit(character1.Damage);
+            txt2.Text = character2.ToString();
+        }
+    }
+}
